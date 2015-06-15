7ca0f4e0cdaf13002a355ea4f24b8eceec9a5024
JensDiemer <git@jensdiemer.de>
2015-06-15 16:35:51 +0200

    test only with supported django versions

--- setup.py
+++ setup.py
@@ -119,7 +119,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,  # include package data under svn source control
     install_requires=[
-        "Django>=1.5",
+        "Django",
     ],
     zip_safe=False,
     classifiers=[
